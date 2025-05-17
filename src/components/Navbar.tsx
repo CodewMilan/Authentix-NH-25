04f3c53d02a23b9c805afcfa766a5df72f3ed541
Milan Sampath <codewmilan@gmail.com>
2025-05-17 18:08:55 +0530

    payments: Added payment options and Razorpay integration

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -41,7 +41,7 @@ const Navbar = () => {
   const navLinks = [
     { name: 'Home', path: '/' },
    
-    { name: 'Explore', path: '/blog' },
+    { name: 'Explore', path: '/explore' },
     { name: 'About Us', path: '/references' },
     { name: 'Contact Us', path: '/booking' },
   ];
